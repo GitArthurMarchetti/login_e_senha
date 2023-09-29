4e137589b5bf37ebcb14db6c298f696a86574abb
ARTHUR D'EÇA MORGENSTERN VINHAS MARCHETTI <arthur_d_marchetti@estudante.sesisenai.org.br>
2023-09-29 12:50:41 -0300

    Atualizado

--- home/script_home.js
+++ home/script_home.js
@@ -1,2 +1,11 @@
 let usuario = JSON.parse(localStorage.getItem("logado"))
 document.querySelector("h1").innerHTML = "Olá " + usuario.login + ", <br> Essa é a home desse lindo site!"
+
+
+function sair(){
+    history.back()
+} 
+
+
+
+
